2fff1854cb243203ed63c1a2abd4d910e5c825a8
yusuftalha37 <109287557+yusuftalha37@users.noreply.github.com>
2023-07-18 22:25:27 +0300

    deneme
    deneme

---
+++ onlinehikayeyazmaca/Assets/Script/Anaekranbutton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Anaekranbutton : MonoBehaviour
+{
+  public void playbutton1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
